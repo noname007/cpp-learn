fe35eeebf002ac72e96a338e9c9bab56803186f6
y.zz <yz0437@gmail.com>
2016-04-10 16:29:20 +0800

    打印对象地址

--- 3/f.cpp
+++ 3/f.cpp
@@ -1,10 +1,13 @@
 
 #include <iostream>
+#include<cstdio>
+
 using namespace std;
 class Base {
 public:
     int k;
-    Base(int n):k(n) { }
+    int v;
+    Base(int n):k(n),v(n*20) { }
 };
 
 class Big  {
@@ -19,5 +22,8 @@ int main()  {
     Big a1(5);    Big a2 = a1;
     cout << a1.v << "," << a1.b.k << endl;
     cout << a2.v << "," << a2.b.k << endl;
+
+    printf("%p %p\n",&(a1.b),&(a2.b));
+    //printf("%p %p\n",(&(a1.b))->v,&(a2.b));
     return 0;
 }
