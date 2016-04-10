b60ac2fc303bba8f1e3c4054adacf9f4ef484f48
y.zz <yz0437@gmail.com>
2016-04-10 15:37:17 +0800

    3.d

---
+++ 3/d.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+class A {
+public:
+    int val;
+
+/*****************************/
+    A():val(0){
+    }
+
+    A(int v){
+        val = v;
+    }
+
+    A& GetObj(){
+
+        return *this;
+    }
+/*****************************/
+
+
+
+};
+int main()  {
+    A a;
+    cout << a.val << endl;
+    a.GetObj() = 5;
+    cout << a.val << endl;
+}
