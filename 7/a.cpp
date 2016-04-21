754fd54fd68d0d97418a118d65f16284e29e21ff
y.zz <yz0437@gmail.com>
2016-04-21 17:36:30 +0800

    7.a.b

---
+++ 7/a.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+template <class T>
+class myclass {
+    /********************My Answer****************************/
+    T *p;
+    int size;
+public:
+
+    myclass(T * arr,int len){
+        p = new T[len+1];
+        int i = 0;
+        for(i =0;i<len;++i){
+
+            p[i] = arr[i];
+        }
+        size = len;
+
+    }
+    /*********************************************************/
+    ~myclass( ) {
+		delete [] p;
+	}
+	void Show()
+	{
+		for( int i = 0;i < size;i ++ ) {
+			cout << p[i] << ",";
+		}
+		cout << endl;
+	}
+};
+int a[100];
+int main() {
+	char line[100];
+	while( cin >> line ) {
+		myclass<char> obj(line,strlen(line));;
+		obj.Show();
+		int n;
+		cin >> n;
+		for(int i = 0;i < n; ++i)
+			cin >> a[i];
+		myclass<int> obj2(a,n);
+		obj2.Show();
+	}
+	return 0;
+}
