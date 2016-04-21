6e1b1056cf032cbff77b5799705df03a91a8aa4a
y.zz <yz0437@gmail.com>
2016-04-21 19:38:24 +0800

    7.c

---
+++ 7/c.cpp
@@ -0,0 +1,65 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+template <class T>
+class CMyistream_iterator
+{
+
+    /********************My Answer****************************/
+    T v[100];
+    int index;
+public:
+
+    CMyistream_iterator(istream &is){
+        T t;
+        cin>>t;
+        v[0] = t;
+        index = 1;
+    }
+
+    void operator++(int){
+        index++;
+    }
+    //friend template<class T1> bool operator *(T1 & i);
+    T operator *(){
+        cin>>v[index];
+        return v[index];
+    }
+};
+//    template<class T>
+//    bool operator *(T & i){
+//
+//    }
+
+class TTTTTTTTT{
+    /*********************************************************/
+};
+
+
+
+int main()
+{
+	int t;
+	cin >> t;
+	while( t -- ) {
+		 CMyistream_iterator<int> inputInt(cin);
+		 int n1,n2,n3;
+		 n1 = * inputInt; //∂¡»Î n1
+		 int tmp = * inputInt;
+		 cout << tmp << endl;
+		 inputInt ++;
+		 n2 = * inputInt; //∂¡»Î n2
+		 inputInt ++;
+		 n3 = * inputInt; //∂¡»Î n3
+		 cout << n1 << " " << n2<< " " << n3 << " ";
+		 CMyistream_iterator<string> inputStr(cin);
+		 string s1,s2;
+		 s1 = * inputStr;
+		 inputStr ++;
+		 s2 = * inputStr;
+		 cout << s1 << " " << s2 << endl;
+	}
+	 return 0;
+}
